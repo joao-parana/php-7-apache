45b29fd089dd425411d860bc4c1a358550d8073a
Joao Antonio Ferreira <joao.parana@gmail.com>
2017-02-14 20:12:13 -0200

    adicionando shell bash de start e run

---
+++ run-cms.sh
@@ -0,0 +1,1 @@
+docker run -i -t  --rm -p 9090:80 -v /Users/admin/Desktop/Development/php-7-apache/site:/var/www/site parana/get-simple-cms
---
+++ start-cms.sh
@@ -0,0 +1,1 @@
+docker run -v  --rm -p 9090:80 -v /Users/admin/Desktop/Development/php-7-apache/site:/var/www/site parana/get-simple-cms
